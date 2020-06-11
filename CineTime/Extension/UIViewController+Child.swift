7fc38b992f022957fbc2511aef18e7b5efe6d3fe
Vinicius Mesquita <viniciuscoelho.ti@gmail.com>
2020-06-11 10:37:01 -0300

    Adding assets to

---
+++ CineTime/Extension/UIViewController+Child.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewController+Child.swift
+//  CineTime
+//
+//  Created by Vinicius Mesquita on 10/06/20.
+//  Copyright © 2020 Beatriz Carlos. All rights reserved.
+//
+
+import Foundation
---
+++ CineTime/View/MyListHeaderView.swift
@@ -0,0 +1,9 @@
+//
+//  MyListHeaderView.swift
+//  CineTime
+//
+//  Created by Vinicius Mesquita on 10/06/20.
+//  Copyright © 2020 Beatriz Carlos. All rights reserved.
+//
+
+import Foundation
