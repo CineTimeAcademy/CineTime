aab03648b17d9f564ca635860edf6c9a46cae386
Leonardo Gomes Fernandes <leonardo-lol252@hotmail.com>
2020-06-15 10:16:14 -0300

    Making Changes in AppDelegate and SceneDelegate

--- CineTime/Config/AppDelegate.swift
+++ CineTime/Config/AppDelegate.swift
@@ -13,14 +13,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         
-        window = UIWindow(frame: UIScreen.main.bounds)
-               
-       let navgationController = UINavigationController(rootViewController: TabBarViewController())
-       window?.rootViewController = navgationController
-       window?.makeKeyAndVisible()
-        
+        if #available(iOS 13.0, *) {
+           
+        } else {
+            window = UIWindow(frame: UIScreen.main.bounds)
+            
+            let navigationController = UINavigationController(rootViewController: TabBarViewController())
+            navigationController.navigationBar.barTintColor = UIColor(red: 0.11, green: 0.11, blue: 0.11, alpha: 1.00)
+            navigationController.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+            
+            navigationController.navigationBar.tintColor = .white
+            window?.rootViewController = navigationController
+            window?.makeKeyAndVisible()
+        }
         return true
     }
 
@@ -41,4 +47,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 }
-
--- CineTime/Config/SceneDelegate.swift
+++ CineTime/Config/SceneDelegate.swift
@@ -19,8 +19,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(frame: UIScreen.main.bounds)
         
-        let navgationController = UINavigationController(rootViewController: TabBarViewController())
-        window?.rootViewController = navgationController
+        let navigationController = UINavigationController(rootViewController: TabBarViewController())
+        navigationController.navigationBar.barTintColor = UIColor(red: 0.11, green: 0.11, blue: 0.11, alpha: 1.00)
+        navigationController.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        
+        navigationController.navigationBar.tintColor = .white
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
         window?.windowScene = windowScene
         
@@ -57,4 +61,3 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
 }
-
