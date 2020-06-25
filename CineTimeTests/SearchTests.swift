d4c7c7aad686331266e7c70cd362e23e39132f90
Brena Amorim <brena.amoriim@gmail.com>
2020-06-25 10:50:23 -0300

    Tentativa de testes  e link da tela

---
+++ CineTimeTests/SearchTests.swift
@@ -0,0 +1,16 @@
+//
+//  SearchTests.swift
+//  CineTimeTests
+//
+//  Created by Brena Amorim on 24/06/20.
+//  Copyright © 2020 Beatriz Carlos. All rights reserved.
+//
+
+import XCTest
+
+@testable import CineTime
+
+class SearchTests: XCTestCase {
+
+    func test_Search
+}
