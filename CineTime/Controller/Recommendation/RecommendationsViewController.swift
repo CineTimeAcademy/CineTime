41b0aae2f1e386601551caf925158c3651ab7720
Beatriz Carlos <beatrizcarlos2000@gmail.com>
2020-06-26 10:10:25 -0300

    auto layout tela de recomendações

--- CineTime/Controller/Recommendation/RecommendationsViewController.swift
+++ CineTime/Controller/Recommendation/RecommendationsViewController.swift
@@ -9,62 +9,73 @@
 import UIKit
 
 class RecommendationsViewController: UIViewController {
-
+    let indicationColletion = RecoCollectionView(frame: .zero, collectionViewLayout: ZoomAndSnapFlowLayout())
+    lazy var NewName: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.textAlignment = .left
+        label.text = "Lançamentos"
+        label.font = UIFont.boldSystemFont(ofSize: 20.0)
+        label.textColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    let newsColletion = NewCollectionView(frame: .zero, collectionViewLayout: HorizontalFlowLayout())
+    
     override func viewWillAppear(_ animated: Bool) {
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.largeTitleDisplayMode = .automatic
-
         navigationController?.viewControllers.first?.navigationItem.title = "Recomendações"
+        
         // Change large title color from rootViewController 
         self.navigationController?.viewControllers.first?             .navigationController?.navigationBar.largeTitleTextAttributes = [                 NSAttributedString.Key.foregroundColor : UIColor.white]
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         view.backgroundColor = .black
-        
-//        let reco = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height / 2))
-//        reco.backgroundColor = .red
         navigationController?.navigationBar.barStyle  = .black
-
         
-        let sizeMinCollection: CGFloat = 256
-        let indicationColletion = RecoCollectionView(frame: CGRect(x: 0, y: 0 , width: view.frame.width, height: view.frame.height - sizeMinCollection), collectionViewLayout: ZoomAndSnapFlowLayout())
         indicationColletion.delegatePush = self
+        indicationColletion.translatesAutoresizingMaskIntoConstraints = false
         indicationColletion.backgroundColor = .black
         view.addSubview(indicationColletion)
-    
-    
-        let newsColletion = NewCollectionView(frame: CGRect(x: 0, y: view.frame.height - sizeMinCollection , width: view.frame.width, height: sizeMinCollection), collectionViewLayout: HorizontalFlowLayout())
+        
         newsColletion.delegatePush = self
         newsColletion.backgroundColor = .black
+        newsColletion.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(newsColletion)
         
-        let NewName: UILabel = {
-            let label = UILabel(frame: CGRect(x: 16, y: view.frame.height - sizeMinCollection, width: 170, height: 34))
-            label.textAlignment = .left
-            label.text = "Lançamentos"
-            label.font = UIFont.boldSystemFont(ofSize: 20.0)
-            label.textColor = .white
-            return label
-        }()
-        
+
         view.addSubview(NewName)
         
+        configureAutoLayout()
     }
     
-
-    /*r
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func configureAutoLayout() {
+        // Recomendações
+        NSLayoutConstraint.activate([
+            indicationColletion.topAnchor.constraint(equalTo: view.topAnchor),
+            indicationColletion.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            indicationColletion.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            indicationColletion.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 6/10)
+        ])
+        
+        // Label
+        NSLayoutConstraint.activate([
+            NewName.topAnchor.constraint(equalTo: indicationColletion.bottomAnchor, constant: 10),
+            NewName.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            NewName.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        
+        //
+        NSLayoutConstraint.activate([
+            newsColletion.topAnchor.constraint(equalTo: NewName.bottomAnchor, constant: -40),
+            newsColletion.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            newsColletion.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            newsColletion.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
-    */
-
 }
 
 // Custom delegate to push the description screen.
